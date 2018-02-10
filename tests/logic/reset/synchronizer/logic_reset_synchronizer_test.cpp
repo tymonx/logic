024a3f86161a474483c4d9158dc85505e6e4d3b0
Tymoteusz Blazejczyk <tymoteusz.blazejczyk.pl@gmail.com>
2018-02-10 22:56:04 +0100

    Improved C++ unit tests for HDL
    * Create single unit test file for reset synchronizer
    * Added target include directories and link libraries
    * Disabled some Clang warnings from Google Tests for unit tests

--- tests/logic/reset/synchronizer/logic_reset_synchronizer_test.cpp
+++ tests/logic/reset/synchronizer/logic_reset_synchronizer_test.cpp
@@ -13,30 +13,64 @@
  * limitations under the License.
  */
 
-#include "dut.hpp"
-#include "test.hpp"
+#include "logic_reset_synchronizer.h"
 
-using logic_reset_synchronizer_test = test;
+#include <gtest/gtest.h>
+#include <logic/gtest/factory.hpp>
+#include <logic/trace.hpp>
+#include <systemc>
+
+class dut {
+public:
+    sc_core::sc_clock aclk{"aclk"};
+    sc_core::sc_signal<bool> areset_n{"areset_n"};
+    sc_core::sc_signal<bool> areset_n_synced{"areset_n_synced"};
+
+    dut() {
+        m_dut.aclk(aclk);
+        m_dut.areset_n(areset_n);
+        m_dut.areset_n_synced(areset_n_synced);
+    }
+private:
+    logic_reset_synchronizer m_dut{"logic_reset_synchronizer"};
+    logic::trace<decltype(m_dut)> m_trace{m_dut};
+};
+
+class logic_reset_synchronizer_test : public ::testing::Test {
+protected:
+    void SetUp() override {
+        m_dut->areset_n = false;
+        sc_start(1, sc_core::SC_NS);
+    }
+
+    void TearDown() override {
+        m_dut->areset_n = false;
+    }
+
+    dut* m_dut{logic::gtest::factory::get<dut>()};
+};
+
+static logic::gtest::factory::add<dut> g;
 
 TEST_F(logic_reset_synchronizer_test, simple) {
-    m_dut->areset_n = 0;
+    m_dut->areset_n = false;
     sc_start(3, SC_NS);
 
     EXPECT_FALSE(m_dut->areset_n_synced.read());
 
-    m_dut->areset_n = 1;
+    m_dut->areset_n = true;
     sc_start(3, SC_NS);
 
     EXPECT_TRUE(m_dut->areset_n_synced.read());
 }
 
 TEST_F(logic_reset_synchronizer_test, deassertion) {
-    m_dut->areset_n = 1;
+    m_dut->areset_n = true;
     sc_start(3, SC_NS);
 
     EXPECT_TRUE(m_dut->areset_n_synced.read());
 
-    m_dut->areset_n = 0;
+    m_dut->areset_n = false;
     sc_start(1, SC_NS);
 
     EXPECT_FALSE(m_dut->areset_n_synced.read());
@@ -44,12 +78,12 @@ TEST_F(logic_reset_synchronizer_test, deassertion) {
 
 TEST_F(logic_reset_synchronizer_test, deassertion_repeat_10) {
     for (auto i = 0u; i < 10u; ++i) {
-        m_dut->areset_n = 1;
+        m_dut->areset_n = true;
         sc_start(3, SC_NS);
 
         EXPECT_TRUE(m_dut->areset_n_synced.read());
 
-        m_dut->areset_n = 0;
+        m_dut->areset_n = false;
         sc_start(1, SC_NS);
 
         EXPECT_FALSE(m_dut->areset_n_synced.read());
