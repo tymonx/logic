8aed4da1115737681ef6d6ecc017448d01abfa43
Tymoteusz Blazejczyk <tymoteusz.blazejczyk.pl@gmail.com>
2018-03-03 10:04:52 +0100

    Added AXI4-Stream tdata byte class

---
+++ include/logic/axi4/stream/tdata_byte.hpp
@@ -0,0 +1,87 @@
+/* Copyright 2018 Tymoteusz Blazejczyk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LOGIC_AXI4_STREAM_TDATA_BYTE_HPP
+#define LOGIC_AXI4_STREAM_TDATA_BYTE_HPP
+
+#include <cstdint>
+#include <utility>
+
+namespace logic {
+namespace axi4 {
+namespace stream {
+
+class tdata_byte {
+public:
+    enum type_t {
+        DATA_BYTE,
+        NULL_BYTE,
+        POSITION_BYTE,
+        RESERVED
+    };
+
+    tdata_byte() noexcept;
+
+    explicit tdata_byte(std::uint8_t data_val) noexcept;
+
+    explicit tdata_byte(type_t type_val) noexcept;
+
+    tdata_byte(std::uint8_t data_val, type_t type_val) noexcept;
+
+    explicit tdata_byte(const std::pair<std::uint8_t, type_t>& value) noexcept;
+
+    tdata_byte& operator=(std::uint8_t data_val) noexcept;
+
+    tdata_byte& operator=(const std::pair<std::uint8_t, type_t>&) noexcept;
+
+    tdata_byte& data(std::uint8_t data_val) noexcept;
+
+    std::uint8_t data() const noexcept;
+
+    tdata_byte& type(type_t type_val) noexcept;
+
+    type_t type() const noexcept;
+
+    explicit operator std::uint8_t() const noexcept;
+
+    explicit operator type_t() const noexcept;
+
+    bool is_data_byte() const noexcept;
+
+    bool is_null_byte() const noexcept;
+
+    bool is_position_byte() const noexcept;
+
+    bool is_reserved() const noexcept;
+
+    tdata_byte(tdata_byte&&) noexcept = default;
+
+    tdata_byte(const tdata_byte&) noexcept = default;
+
+    tdata_byte& operator=(tdata_byte&&) noexcept = default;
+
+    tdata_byte& operator=(const tdata_byte&) noexcept = default;
+
+    ~tdata_byte() noexcept = default;
+private:
+    type_t m_type;
+    std::uint8_t m_data;
+};
+
+} /* namespace stream */
+} /* namespace axi4 */
+} /* namespace logic */
+
+#endif /* LOGIC_AXI4_STREAM_TDATA_BYTE_HPP */
---
+++ src/logic/axi4/stream/tdata_byte.cpp
@@ -0,0 +1,97 @@
+/* Copyright 2018 Tymoteusz Blazejczyk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "logic/axi4/stream/tdata_byte.hpp"
+
+using logic::axi4::stream::tdata_byte;
+
+tdata_byte::tdata_byte() noexcept :
+    m_type{DATA_BYTE},
+    m_data{0}
+{ }
+
+tdata_byte::tdata_byte(std::uint8_t data_val) noexcept :
+    m_type{DATA_BYTE},
+    m_data{data_val}
+{ }
+
+tdata_byte::tdata_byte(type_t type_val) noexcept :
+    m_type{type_val},
+    m_data{0}
+{ }
+
+tdata_byte::tdata_byte(std::uint8_t data_val, type_t type_val) noexcept :
+    m_type{type_val},
+    m_data{data_val}
+{ }
+
+tdata_byte::tdata_byte(const std::pair<std::uint8_t, type_t>& value) noexcept :
+    m_type{value.second},
+    m_data{value.first}
+{ }
+
+auto tdata_byte::operator=(std::uint8_t data_val) noexcept -> tdata_byte& {
+    m_data = data_val;
+    return *this;
+}
+
+auto tdata_byte::operator=(
+        const std::pair<std::uint8_t, type_t>& value) noexcept -> tdata_byte& {
+    m_type = value.second;
+    m_data = value.first;
+    return *this;
+}
+
+auto tdata_byte::data(std::uint8_t data_val) noexcept -> tdata_byte& {
+    m_data = data_val;
+    return *this;
+}
+
+auto tdata_byte::data() const noexcept -> std::uint8_t {
+    return m_data;
+}
+
+auto tdata_byte::type(type_t type_val) noexcept -> tdata_byte& {
+    m_type = type_val;
+    return *this;
+}
+
+auto tdata_byte::type() const noexcept -> type_t {
+    return m_type;
+}
+
+tdata_byte::operator std::uint8_t() const noexcept {
+    return m_data;
+}
+
+tdata_byte::operator type_t() const noexcept {
+    return m_type;
+}
+
+bool tdata_byte::is_data_byte() const noexcept {
+    return (DATA_BYTE == m_type);
+}
+
+bool tdata_byte::is_null_byte() const noexcept {
+    return (NULL_BYTE == m_type);
+}
+
+bool tdata_byte::is_position_byte() const noexcept {
+    return (POSITION_BYTE == m_type);
+}
+
+bool tdata_byte::is_reserved() const noexcept {
+    return (RESERVED == m_type);
+}
