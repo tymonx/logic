a881e7360725f54c0873771669ff84debb2b3370
Tymoteusz Blazejczyk <tymoteusz.blazejczyk.pl@gmail.com>
2018-03-06 22:57:04 +0100

    Fixed Google Test installation for Travis CI

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -17,198 +17,216 @@ set -e
 set -u
 
 function install_systemc {
-    if [ ! -f "$SYSTEMC_ROOT/$SYSTEMC_TAR" ]; then
+    if [ ! -f "$SYSTEMC_ARCHIVE_DIR/$SYSTEMC_TAR" ]; then
         echo "Downloading $SYSTEMC_URL/$SYSTEMC_TAR..."
-        wget $SYSTEMC_URL/$SYSTEMC_TAR -O $SYSTEMC_ROOT/$SYSTEMC_TAR
+        wget $SYSTEMC_URL/$SYSTEMC_TAR -O $SYSTEMC_ARCHIVE_DIR/$SYSTEMC_TAR
     else
         echo "SystemC $SYSTEMC_URL/$SYSTEMC_TAR already downloaded"
     fi
 
-    echo "Creating SystemC directories..."
-    mkdir -p /tmp/systemc/$SYSTEMC_VERSION
-    mkdir -p /tmp/src/systemc/$SYSTEMC_VERSION
-    mkdir -p /tmp/src/systemc/$SYSTEMC_VERSION/build
+    if [ ! -f "$INSTALL/systemc/$SYSTEMC_VERSION/include/systemc.h" ];then
+        echo "Creating SystemC directories..."
+        mkdir -p $INSTALL/systemc/$SYSTEMC_VERSION
+        mkdir -p /tmp/src/systemc/$SYSTEMC_VERSION
+        mkdir -p /tmp/src/systemc/$SYSTEMC_VERSION/build
 
-    echo "Unpacking SystemC archive file..."
-    tar -xf $SYSTEMC_ROOT/$SYSTEMC_TAR \
-        -C /tmp/src/systemc/$SYSTEMC_VERSION --strip-components 1
+        echo "Unpacking SystemC archive file..."
+        tar -xf $SYSTEMC_ARCHIVE_DIR/$SYSTEMC_TAR \
+            -C /tmp/src/systemc/$SYSTEMC_VERSION --strip-components 1
 
-    echo "Patching SystemC sources..."
-    cd /tmp/src/systemc/$SYSTEMC_VERSION
-    sed -i 's/nb_put/this->nb_put/' \
-        src/tlm_core/tlm_1/tlm_analysis/tlm_analysis_fifo.h
+        echo "Patching SystemC sources..."
+        cd /tmp/src/systemc/$SYSTEMC_VERSION
+        sed -i 's/nb_put/this->nb_put/' \
+            src/tlm_core/tlm_1/tlm_analysis/tlm_analysis_fifo.h
 
-    echo "Configuring SystemC sources..."
-    cd /tmp/src/systemc/$SYSTEMC_VERSION/build
-    ../configure --enable-pthreads --enable-shared \
-        --prefix=/tmp/systemc/$SYSTEMC_VERSION
+        echo "Configuring SystemC sources..."
+        cd /tmp/src/systemc/$SYSTEMC_VERSION/build
+        ../configure --enable-pthreads --enable-shared \
+            --prefix=$INSTALL/systemc/$SYSTEMC_VERSION
 
-    echo "Building SystemC library..."
-    make -j`nproc`
+        echo "Building SystemC library..."
+        make -j`nproc`
 
-    echo "Installing SystemC library..."
-    make install
+        echo "Installing SystemC library..."
+        make install
+    else
+        echo "SystemC library already installed"
+    fi
 
-    export SYSTEMC_INCLUDE=/tmp/systemc/$SYSTEMC_VERSION/include
-    export SYSTEMC_LIBDIR=/tmp/systemc/$SYSTEMC_VERSION/lib-linux64
+    export SYSTEMC_INCLUDE=$INSTALL/systemc/$SYSTEMC_VERSION/include
+    export SYSTEMC_LIBDIR=$INSTALL/systemc/$SYSTEMC_VERSION/lib-linux64
 }
 
 function install_uvm_systemc {
-    if [ ! -f "$UVM_SYSTEMC_ROOT/$UVM_SYSTEMC_TAR" ]; then
+    if [ ! -f "$UVM_SYSTEMC_ARCHIVE_DIR/$UVM_SYSTEMC_TAR" ]; then
         echo "Downloading $UVM_SYSTEMC_URL/$UVM_SYSTEMC_TAR..."
         wget $UVM_SYSTEMC_URL/$UVM_SYSTEMC_TAR \
-            -O $UVM_SYSTEMC_ROOT/$UVM_SYSTEMC_TAR
+            -O $UVM_SYSTEMC_ARCHIVE_DIR/$UVM_SYSTEMC_TAR
     else
         echo "UVM-SystemC $UVM_SYSTEMC_URL/$UVM_SYSTEMC_TAR already downloaded"
     fi
 
-    echo "Creating UVM-SystemC directories..."
-    mkdir -p /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION
-    mkdir -p /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION/build
+    if [ ! -f "$INSTALL/systemc/$SYSTEMC_VERSION/include/uvm.h" ]; then
+        echo "Creating UVM-SystemC directories..."
+        mkdir -p /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION
+        mkdir -p /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION/build
 
-    echo "Unpacking UVM-SystemC archive file..."
-    tar -xf $UVM_SYSTEMC_ROOT/$UVM_SYSTEMC_TAR \
-        -C /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION --strip-components 1
+        echo "Unpacking UVM-SystemC archive file..."
+        tar -xf $UVM_SYSTEMC_ARCHIVE_DIR/$UVM_SYSTEMC_TAR \
+            -C /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION --strip-components 1
 
-    echo "Creating UVM-SystemC configure file..."
-    cd /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION
-    ./config/bootstrap
+        echo "Creating UVM-SystemC configure file..."
+        cd /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION
+        ./config/bootstrap
 
-    echo "Configuring UVM-SystemC sources..."
-    cd /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION/build
-    ../configure --enable-shared --with-systemc=/tmp/systemc/$SYSTEMC_VERSION \
-        --prefix=/tmp/systemc/$SYSTEMC_VERSION
+        echo "Configuring UVM-SystemC sources..."
+        cd /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION/build
+        ../configure --enable-shared --with-systemc=$INSTALL/systemc/$SYSTEMC_VERSION \
+            --prefix=$INSTALL/systemc/$SYSTEMC_VERSION
 
-    echo "Building UVM-SystemC library..."
-    make -j`nproc`
+        echo "Building UVM-SystemC library..."
+        make -j`nproc`
 
-    echo "Installing UVM-SystemC library..."
-    make install
+        echo "Installing UVM-SystemC library..."
+        make install
+    else
+        echo "UVM-SystemC library already installed"
+    fi
 }
 
 function install_scv {
-    if [ ! -f "$SCV_ROOT/$SCV_TAR" ]; then
+    if [ ! -f "$SCV_ARCHIVE_DIR/$SCV_TAR" ]; then
         echo "Downloading $SCV_URL/$SCV_TAR..."
-        wget $SCV_URL/$SCV_TAR -O $SCV_ROOT/$SCV_TAR
+        wget $SCV_URL/$SCV_TAR -O $SCV_ARCHIVE_DIR/$SCV_TAR
     else
         echo "SCV $SCV_URL/$SCV_TAR already downloaded"
     fi
 
-    echo "Creating SCV directories..."
-    mkdir -p /tmp/src/scv/$SCV_VERSION
-    mkdir -p /tmp/src/scv/$SCV_VERSION/build
+    if [ ! -f "$INSTALL/systemc/$SYSTEMC_VERSION/include/scv.h" ]; then
+        echo "Creating SCV directories..."
+        mkdir -p /tmp/src/scv/$SCV_VERSION
+        mkdir -p /tmp/src/scv/$SCV_VERSION/build
 
-    echo "Unpacking SCV archive file..."
-    tar -xf $SCV_ROOT/$SCV_TAR \
-        -C /tmp/src/scv/$SCV_VERSION --strip-components 1
+        echo "Unpacking SCV archive file..."
+        tar -xf $SCV_ARCHIVE_DIR/$SCV_TAR \
+            -C /tmp/src/scv/$SCV_VERSION --strip-components 1
 
-    echo "Configuring SCV sources..."
-    cd /tmp/src/scv/$SCV_VERSION/build
-    ../configure --enable-shared --with-systemc=/tmp/systemc/$SYSTEMC_VERSION \
-        --prefix=/tmp/systemc/$SYSTEMC_VERSION
+        echo "Configuring SCV sources..."
+        cd /tmp/src/scv/$SCV_VERSION/build
+        ../configure --enable-shared --with-systemc=$INSTALL/systemc/$SYSTEMC_VERSION \
+            --prefix=$INSTALL/systemc/$SYSTEMC_VERSION
 
-    echo "Building SCV library..."
-    make -j`nproc`
+        echo "Building SCV library..."
+        make -j`nproc`
 
-    echo "Installing SCV library..."
-    make install
+        echo "Installing SCV library..."
+        make install
+    else
+        echo "SCV library already installed"
+    fi
 }
 
 function install_verilator {
-    if [ ! -f "$VERILATOR_ROOT/$VERILATOR_TAR" ]; then
+    if [ ! -f "$VERILATOR_ARCHIVE_DIR/$VERILATOR_TAR" ]; then
         echo "Downloading $VERILATOR_URL/$VERILATOR_TAR..."
-        wget $VERILATOR_URL/$VERILATOR_TAR -O $VERILATOR_ROOT/$VERILATOR_TAR
+        wget $VERILATOR_URL/$VERILATOR_TAR \
+            -O $VERILATOR_ARCHIVE_DIR/$VERILATOR_TAR
     else
         echo "Verilator $VERILATOR_URL/$VERILATOR_TAR already downloaded"
     fi
 
-    echo "Creating Verilator directories..."
-    mkdir -p /tmp/verilator/$VERILATOR_VERSION
-    mkdir -p /tmp/src/verilator/$VERILATOR_VERSION
+    if [ ! -x "$INSTALL/verilator/$VERILATOR_VERSION/bin/verilator" ]; then
+        echo "Creating Verilator directories..."
+        mkdir -p $INSTALL/verilator/$VERILATOR_VERSION
+        mkdir -p /tmp/src/verilator/$VERILATOR_VERSION
 
-    echo "Unpacking Verilator archive file..."
-    tar -xzf $VERILATOR_ROOT/$VERILATOR_TAR \
-        -C /tmp/src/verilator/$VERILATOR_VERSION --strip-components 1
+        echo "Unpacking Verilator archive file..."
+        tar -xzf $VERILATOR_ARCHIVE_DIR/$VERILATOR_TAR \
+            -C /tmp/src/verilator/$VERILATOR_VERSION --strip-components 1
 
-    echo "Installing Verilator dependencies..."
-    sudo apt-get install git make autoconf g++ flex bison -y
+        echo "Installing Verilator dependencies..."
+        sudo apt-get install git make autoconf g++ flex bison -y
 
-    echo "Configuring Verilator sources..."
-    cd /tmp/src/verilator/$VERILATOR_VERSION
-    ./configure --prefix=/tmp/verilator/$VERILATOR_VERSION
+        echo "Configuring Verilator sources..."
+        cd /tmp/src/verilator/$VERILATOR_VERSION
+        ./configure --prefix=$INSTALL/verilator/$VERILATOR_VERSION
 
-    echo "Building Verilator library..."
-    make -j`nproc`
+        echo "Building Verilator library..."
+        make -j`nproc`
 
-    echo "Installing Verilator library..."
-    make install
+        echo "Installing Verilator library..."
+        make install
+    else
+        echo "Verilator already installed"
+    fi
 
-    export VERILATOR_ROOT=/tmp/verilator/$VERILATOR_VERSION
+    export VERILATOR_ROOT=$INSTALL/verilator/$VERILATOR_VERSION
 }
 
 function install_gtest {
-    if [ ! -f "$GTEST_ROOT/$GTEST_TAR" ]; then
-        echo "Downloading $GTEST_URL/$GTEST_TAR..."
-        wget $GTEST_URL/$GTEST_TAR -O $GTEST_ROOT/$GTEST_TAR
+    if [ ! -f "$INSTALL/gtest/$GTEST_VERSION/include/gtest/gtest.h" ]; then
+        echo "Creating Google Test directories..."
+        mkdir -p $INSTALL/gtest/$GTEST_VERSION
+        mkdir -p /tmp/src/gtest/$GTEST_VERSION
+        mkdir -p /tmp/src/gtest/$GTEST_VERSION/build
+
+        echo "Downloading Google Test sources..."
+        cd /tmp/src/gtest/$GTEST_VERSION
+        git clone https://github.com/google/googletest.git build
+        git checkout 703b4a8
+
+        echo "Configuring Google Test sources..."
+        cd /tmp/src/gtest/$GTEST_VERSION/build
+        cmake -DBUILD_SHARED_LIBS=ON \
+            -DCMAKE_INSTALL_PREFIX=$INSTALL/gtest/$GTEST_VERSION ..
+
+        echo "Building Google Test library..."
+        cmake --build . --target all -- -j`nproc`
+
+        echo "Installing Google Test library..."
+        cmake --build . --target install
     else
-        echo "Google Test $GTEST_URL/$GTEST_TAR already downloaded"
+        echo "Google Test library already installed"
     fi
 
-    echo "Creating Google Test directories..."
-    mkdir -p /tmp/gtest/$GTEST_VERSION
-    mkdir -p /tmp/src/gtest/$GTEST_VERSION
-    mkdir -p /tmp/src/gtest/$GTEST_VERSION/build
-
-    echo "Unpacking Google Test archive file..."
-    uzip $GTEST_ROOT/$GTEST_TAR -d /tmp/src/gtest
-
-    echo "Configuring Google Test sources..."
-    cd /tmp/src/gtest/$GTEST_VERSION/build
-    cmake -DBUILD_SHARED_LIBS=ON \
-        -DCMAKE_INSTALL_PREFIX=/tmp/gtest/$GTEST_VERSION ..
-
-    echo "Building Google Test library..."
-    cmake --build . --target all -- -j`nproc`
-
-    echo "Installing Google Test library..."
-    cmake --build . --target install
-
-    export GTEST_ROOT=/tmp/gtest/$GTEST_VERSION
+    export GTEST_ROOT=$INSTALL/gtest/$GTEST_VERSION
 }
 
 echo "Preparing tools..."
 
 sudo apt-get update -qq
 
-SYSTEMC_ROOT=$HOME/systemc
+INSTALL=$HOME/tools
+
+SYSTEMC_ARCHIVE_DIR=$HOME/archive/systemc
 SYSTEMC_VERSION=2.3.2
 SYSTEMC_URL=http://accellera.org/images/downloads/standards/systemc
 SYSTEMC_TAR=systemc-$SYSTEMC_VERSION.tar.gz
 
 install_systemc
 
-UVM_SYSTEMC_ROOT=$HOME/uvm-systemc
+UVM_SYSTEMC_ARCHIVE_DIR=$HOME/archive/uvm-systemc
 UVM_SYSTEMC_VERSION=1.0-beta1
 UVM_SYSTEMC_URL=http://accellera.org/images/downloads/standards/systemc
 UVM_SYSTEMC_TAR=uvm-systemc-$UVM_SYSTEMC_VERSION.tar.gz
 
 install_uvm_systemc
 
-SCV_ROOT=$HOME/scv
+SCV_ARCHIVE_DIR=$HOME/archive/scv
 SCV_VERSION=2.0.1
 SCV_URL=http://accellera.org/images/downloads/standards/systemc
 SCV_TAR=scv-$SCV_VERSION.tar.gz
 
 install_scv
 
-VERILATOR_ROOT=$HOME/verilator
+VERILATOR_ARCHIVE_DIR=$HOME/archive/verilator
 VERILATOR_VERSION=3.920
 VERILATOR_URL=https://www.veripool.org/ftp
 VERILATOR_TAR=verilator-$VERILATOR_VERSION.tgz
 
 install_verilator
 
-GTEST_ROOT=$HOME/gtest
+GTEST_ROOT=$HOME/archive/gtest
 GTEST_VERSION=master
 GTEST_URL=https://github.com/google/googletest/archive
 GTEST_TAR=$GTEST_VERSION.zip
