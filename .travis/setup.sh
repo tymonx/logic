07b393367424d55cf2bd64b28e5b7ba2a34dd785
Tymoteusz Blazejczyk <tymoteusz.blazejczyk.pl@gmail.com>
2018-03-06 22:00:55 +0100

    Fixed default CC and CXX variables in Travis CI

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -67,7 +67,7 @@ function install_uvm_systemc {
     mkdir -p /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION/build
 
     echo "Unpacking UVM-SystemC archive file..."
-    tar -xf $UVM_SYSTEMC_ROOT/$UVM_SYSTEMC_TAR \
+    tar -xvf $UVM_SYSTEMC_ROOT/$UVM_SYSTEMC_TAR \
         -C /tmp/src/uvm-systemc/$UVM_SYSTEMC_VERSION --strip-components 1
 
     echo "Configuring UVM-SystemC sources..."
